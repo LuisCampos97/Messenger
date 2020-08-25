3f307d9d5e25cc1a1ee133752a728dbf4cc0eb7a
Luís Campos <luismmc1997@hotmail.com>
2020-08-25 17:06:51 +0100

    Reorder posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Post;
 use App\User;
 use DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -16,8 +17,18 @@ class PostController extends Controller
 
     public function createPost(Request $request)
     {
+        $myPosts = Post::where('user_id', '=', Auth::id());
+
         $post = new Post();
         $post->body = $request->body;
+
+        if ($myPosts->count() == 0) {
+            $post->order = 1;
+        } else {
+            $lastPost = $myPosts->orderBy('order', 'desc')->first();
+            $post->order = $lastPost->order + 1;
+        }
+
         $request->user()->posts()->save($post);
 
         return redirect()->route('home')->with('success', 'Post successfully created!');
@@ -28,23 +39,68 @@ class PostController extends Controller
         $posts = DB::table('posts')
             ->join('users', 'posts.user_id', '=', 'users.id')
             ->select('posts.*', 'users.name')
-            ->orderBy('created_at', 'desc')
+        //->orderBy('created_at', 'desc')
+            ->orderBy('order', 'desc')
             ->get();
 
-        return view('home', ['posts' => $posts]);
+        $postsCount = Post::where('user_id', '=', Auth::id())->count();
+
+        return view('home', ['posts' => $posts, 'postsCount' => $postsCount]);
     }
 
     public function deletePost($post_id)
     {
         $post = Post::find($post_id);
+
+        $myPosts = Post::where('user_id', '=', Auth::id());
+
+        $postsToRemove = $myPosts->where('order', '>', $post->order)->get();
+
+        if ($post->order != $myPosts->count() && $postsToRemove->count() != 1) {
+            dd("OLA");
+            foreach ($myPosts as $p) {
+                $p->order--;
+                dd($p);
+                $p->save();
+            }
+        }
+
         $post->delete();
 
         return redirect()->route('home');
     }
 
-    public function reorderPost($post_id)
+    public function moveUpOrder($post_id)
     {
-        dd($post_id);
+        $myPosts = Post::where('user_id', '=', Auth::id());
 
+        $post = Post::find($post_id);
+        $number = $post->order;
+
+        $postToOrder = $myPosts->where('order', $number + 1)->first();
+        $postToOrder->order--;
+        $postToOrder->save();
+
+        $post->order++;
+        $post->save();
+
+        return redirect()->route('home');
+    }
+
+    public function moveDownOrder($post_id)
+    {
+        $myPosts = Post::where('user_id', '=', Auth::id());
+
+        $post = Post::find($post_id);
+        $number = $post->order;
+
+        $postToOrder = $myPosts->where('order', $number - 1)->first();
+        $postToOrder->order++;
+        $postToOrder->save();
+
+        $post->order--;
+        $post->save();
+
+        return redirect()->route('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,14 +38,22 @@
                         <div class="container">
                             @if (Auth::id() == $post->user_id)
                             <div class="float-right">
-                                <form action="{{ action('PostController@reorderPost', $post->id) }}" method="PUT">
-                                    @csrf @method('put')
-                                    <button type="submit" class="btn btn-default"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 5a.5.5 0 0 0-1 0v4.793L5.354 7.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 9.793V5z"/>
-                                    </svg></button>
+                                <form action="{{ action('PostController@moveDownOrder', $post->id) }}" method="POST">
+                                    @csrf @method('patch')
+                                    @if ($post->order != 1)
+                                        <button type="submit" class="btn btn-default"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 5a.5.5 0 0 0-1 0v4.793L5.354 7.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 9.793V5z"/>
+                                        </svg></button>
+                                    @endif
+                                </form>
+                                <form action="{{ action('PostController@moveUpOrder', $post->id) }}" method="POST">
+                                    @csrf @method('patch')
+                                    @if ($post->order != $postsCount)
                                     <button type="submit" class="btn btn-default"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 8.354a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 6.207V11a.5.5 0 0 1-1 0V6.207L5.354 8.354z"/>
                                     </svg></button>
+                                    @endif
+                                    
                                 </form>
                             </div>
                             @endif
